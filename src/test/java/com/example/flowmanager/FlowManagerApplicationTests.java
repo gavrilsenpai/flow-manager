a4dc7df189e56c5ec69e04de8e3a1fb5efef1556
gavrilsenpai <19oxlopkov99@gmail.com>
2026-07-30 16:25:43 +0300

    Initial commit

---
+++ src/test/java/com/example/flowmanager/FlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
